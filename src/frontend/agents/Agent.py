d096a7ecea2602949a501190bc82ad87ad388d97
sl-badcoder <senadlg01@gmail.com>
2026-05-17 18:07:33 +0200

    added specific agent definition

---
+++ src/frontend/agents/Agent.py
@@ -0,0 +1,13 @@
+import asyncio
+class Agent:
+    def __init__(self) -> None:
+        self.type = ""
+
+
+    async def run(self, input: str) -> str | None:
+        pass
+
+    def get_type(self) -> str:
+        return self.type
+
+    
